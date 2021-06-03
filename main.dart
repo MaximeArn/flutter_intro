f17e1cf7b7f9fdfa62f3d1665b8cc3368874b7cb
MaximeArn <maxime-arnould@outlook.fr>
2021-06-03 23:22:09 +0200

    dart Map and its principal methods

--- main.dart
+++ main.dart
@@ -1,33 +1,10 @@
 main() {
-  var list = [1, 2, 3, 3, 3, 4, "str", true];
-  // list is typed by inference as in typescript
-  List<double> list2 = [
-    3.4,
-    24,
-    5,
-  ];
-  List<Object> list3 = [
-    1,
-    "str",
-    true,
-    [1, 2, 3]
-  ];
-  //list3 will accept all types because all types extends from Object class
-  list3.add(5);
-  List<int> intList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  intList.remove(3);
-  print(intList);
-
-  //remove a element
-  intList.removeAt(0);
-
-  print(intList);
-  //remove at an index
-
-  intList.forEach((element) {
-    print(element == 4);
-  });
-
-  var squareIntList = intList.map((e) => e*e);
-  print(squareIntList);
+  Map<String, Object> pet = {"name": "john", "age": 25};
+  print(pet);
+  print(pet.length);
+  print(pet["name"]);
+  print(pet.keys);
+  print(pet.values);
+  print(pet.entries);
+  pet["color"] = "blue";
 }
