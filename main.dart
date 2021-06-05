73c8778f8c4ca685022ce8038ef7fe56315f235d
MaximeArn <maxime-arnould@outlook.fr>
2021-06-05 16:01:39 +0200

    generic type

--- main.dart
+++ main.dart
@@ -10,17 +10,21 @@ class Human {
   }
 }
 
-class User extends Human {
+class User<T> extends Human {
   String name;
   String password;
+  T preferedType;
   String get cryptedPassword {
     return (this.password + " crypted");
   }
 
-  User({required String this.name, required String this.password})
+  User(
+      {required String this.name,
+      required String this.password,
+      required this.preferedType})
       : super(sex: "male") {}
 
-@override
+  @override
   void speak() {
     print("hello I am a user  !");
   }
@@ -28,13 +32,18 @@ class User extends Human {
   static void speakFrench() {
     print("bonjour toi !");
   }
+
 }
 
 main(List<String> args) {
-  User john = User(name: "john", password: "12345");
+  User<String> john =
+      User(name: "john", password: "12345", preferedType: "strings");
 
   print(john.cryptedPassword);
   john.speak();
+
+  //generic typed prop
+  print(john.preferedType.runtimeType); //String
 
   //herited property
   print(john.eys);
