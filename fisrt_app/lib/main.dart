df1424a60a825ef266dc5f4b77dd68e7d3eda244
MaximeArn <maxime-arnould@outlook.fr>
2021-06-07 13:29:03 +0200

    Image widget using image url

--- fisrt_app/lib/main.dart
+++ fisrt_app/lib/main.dart
@@ -8,28 +8,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text.rich(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: "Coo"
-              ),
-              TextSpan(
-                text: "K",
-                style: TextStyle(
-                  color: Color.fromRGBO(255,215,0,1),
-                ),
-              ),
-              TextSpan(
-                text: "ing"
-              )
-            ]),
-          textDirection: TextDirection.ltr,
-        style: TextStyle(
-         fontSize: 50,
-         fontWeight: FontWeight.w500
-        ),
-            )
+        child: Image(
+            image: NetworkImage(
+                "https://images.ctfassets.net/23aumh6u8s0i/4TsG2mTRrLFhlQ9G1m19sC/4c9f98d56165a0bdd71cbe7b9c2e2484/flutter"))
+        //     Text.rich(
+        //   TextSpan(children: [
+        //     TextSpan(text: "Coo"),
+        //     TextSpan(
+        //       text: "K",
+        //       style: TextStyle(
+        //         color: Color.fromRGBO(255, 215, 0, 1),
+        //       ),
+        //     ),
+        //     TextSpan(text: "ing")
+        //   ]),
+        //   textDirection: TextDirection.ltr,
+        //   style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
+        // )
         );
   }
 }
