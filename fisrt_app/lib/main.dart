998f23e2b434a1b3050e4939a7d6defabc5f7f5e
MaximeArn <maxime-arnould@outlook.fr>
2021-06-07 13:38:52 +0200

    Assets and AssetImage widget

--- fisrt_app/lib/main.dart
+++ fisrt_app/lib/main.dart
@@ -9,22 +9,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Image(
-            image: NetworkImage(
-                "https://images.ctfassets.net/23aumh6u8s0i/4TsG2mTRrLFhlQ9G1m19sC/4c9f98d56165a0bdd71cbe7b9c2e2484/flutter"))
-        //     Text.rich(
-        //   TextSpan(children: [
-        //     TextSpan(text: "Coo"),
-        //     TextSpan(
-        //       text: "K",
-        //       style: TextStyle(
-        //         color: Color.fromRGBO(255, 215, 0, 1),
-        //       ),
-        //     ),
-        //     TextSpan(text: "ing")
-        //   ]),
-        //   textDirection: TextDirection.ltr,
-        //   style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
-        // )
-        );
+      image: AssetImage("assets/flutter.png"),
+      width: 300,
+      height: 100,
+      color: Colors.yellow.shade400,
+      //     Text.rich(
+      //   TextSpan(children: [
+      //     TextSpan(text: "Coo"),
+      //     TextSpan(
+      //       text: "K",
+      //       style: TextStyle(
+      //         color: Color.fromRGBO(255, 215, 0, 1),
+      //       ),
+      //     ),
+      //     TextSpan(text: "ing")
+      //   ]),
+      //   textDirection: TextDirection.ltr,
+      //   style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
+      // )
+    ));
   }
 }
