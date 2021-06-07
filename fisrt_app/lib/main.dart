aedcc5a41b06d0285aee335f67f6d26b264dbe25
MaximeArn <maxime-arnould@outlook.fr>
2021-06-07 18:41:02 +0200

    Container Widget

--- fisrt_app/lib/main.dart
+++ fisrt_app/lib/main.dart
@@ -7,26 +7,35 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Image(
-      image: AssetImage("assets/flutter.png"),
-      width: 300,
-      height: 100,
-      color: Colors.yellow.shade400,
-      //     Text.rich(
-      //   TextSpan(children: [
-      //     TextSpan(text: "Coo"),
-      //     TextSpan(
-      //       text: "K",
-      //       style: TextStyle(
-      //         color: Color.fromRGBO(255, 215, 0, 1),
-      //       ),
-      //     ),
-      //     TextSpan(text: "ing")
-      //   ]),
-      //   textDirection: TextDirection.ltr,
-      //   style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
-      // )
-    ));
+    return Column(
+      textDirection: TextDirection.ltr,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+         height: 150,
+          width: 150
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.green,
+            width: 150
+          ),
+        ),
+        Expanded(child: 
+        Container(
+          color: Colors.blue,
+          height: 150,
+          width: 150
+        ),
+        ),
+        Container(
+          color: Colors.yellow,
+         height: 150,
+          width: 150
+        )
+      ],
+    );
   }
 }
